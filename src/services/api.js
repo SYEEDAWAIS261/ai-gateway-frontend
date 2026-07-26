779947cb1f420384bb1af231f75979bc2ec2f4a3
SYEEDAWAIS261 <awaisshah14101@icloud.com>
2026-07-26 12:53:38 +0500

    add last commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api/v1',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api/v1',
 });
 
 // Pass Auth token automatically
